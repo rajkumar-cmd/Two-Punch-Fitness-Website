678b1b41aabbfdef9d2d87b9f58da57f4b2f6c8f
rajkumar-cmd <raymarhossor@gmail.com>
2022-11-08 16:30:56 +0530

    Responsive Footer Components

---
+++ Components/footer.js
@@ -0,0 +1,63 @@
+const footer=()=>{
+    return `<div id="footerItems">
+    <div>
+        <div id="footerLogo"><img src="https://cloudfront.fitnessblender.com/assets/img/logos/fb-logo-dark.svg" alt=""></div>
+        <div id="footerLogoBase">WORKOUT COMPLETE™</div>
+    </div>
+    <div id="footerElements">
+        <div>
+            <div class="footerTitle"><h5>WORKOUTS</h5></div>
+            <div class="footerList">WORKOUT VIDEOS</div>
+            <div class="footerList">CUSTOM WORKOUTS</div>
+            <div class="footerTitle"><h5>PROGRAMS</h5></div>
+            <div class="footerList">WORKOUT PROGRAMS</div>
+            <div class="footerList">MEAL PLANS</div>
+            <div class="footerList">PILOT PROGRAMS</div>
+            <div class="footerList">ROUTINES</div>
+        </div>
+        <div>
+            <div class="footerTitle"><h5>HEALTHY LIVING</h5></div>
+            <div class="footerList">FITNESS</div>
+            <div class="footerList">HEALTH</div>
+            <div class="footerList">NUTRITION</div>
+            <div class="footerList">HEALTHY RECIPES</div>
+            <div class="footerList">EXPERTS</div>
+        </div>
+        <div>
+            <div class="footerTitle"><h5>ABOUT</h5></div>
+            <div class="footerList">CAREERS</div>
+            <div class="footerList">TUTORIALS</div>
+            <div class="footerList">OUR TEAM</div>
+            <div class="footerList">B2B OPTIONS</div>
+        </div>
+        <div>
+            <div class="footerTitle"><h5>MEMBERSHIP</h5></div>
+            <div class="footerTitle"><h5>FB PLUS</h5></div>
+            <div class="footerTitle"><h5>COMMUNITY</h5></div>
+            <div class="footerTitle"><h5>BLOG</h5></div>
+            <div class="footerTitle"><h5>CONTACT US</h5></div>
+            <div class="footerTitle"><h5>FAQ</h5></div>
+            <div class="footerTitle"><h5>STORE</h5></div>
+        </div>
+    </div>
+</div>
+<div id="lowerFooter">
+    <div><hr></div>
+    <div id="lowerFooterElement">
+        <div id="lowerFooterElementText">
+            <div>Copyright © 2022 Fitness Blender. All rights reserved.</div>
+            <div>Terms of Use</div>
+            <div>Privacy Policy</div>
+        </div>
+    </div>
+    <div id="lowerFooterElementIcon">
+        <div class="logoFooter" id="footerYoutube"><img src="https://img.icons8.com/external-those-icons-flat-those-icons/512/external-YouTube-Logo-social-media-those-icons-flat-those-icons.png" alt=""></div>
+        <div class="logoFooter" id="footerPinterest"><img src="https://img.icons8.com/color/512/pinterest.png" alt=""></div>
+        <div class="logoFooter" id="footerFacebook"><img src="https://img.icons8.com/color/512/facebook-new.png" alt=""></div>
+        <div class="logoFooter" id="footerInstagram"><img src="https://img.icons8.com/fluency/512/instagram-new.png" alt=""></div>
+        <div class="logoFooter" id="footerTwitter"><img src="https://img.icons8.com/color/512/twitter.png" alt=""></div>
+    </div>
+</div>`
+}
+
+export default footer;
