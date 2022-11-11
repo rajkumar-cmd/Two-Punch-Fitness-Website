0a9e921845f3863b440c7cb53839de7ec4273f45
shagunsharma6677 <shagunsharma6677@gmail.com>
2022-11-11 19:50:01 +0530

    Shagun update-4

--- scripts/workout-Program.js
+++ scripts/workout-Program.js
@@ -35,57 +35,64 @@ let fetchData = async () => {
     let res = await fetch(`http://localhost:3000/workout-programs`);
     let data = await res.json();
     console.log(data);
-    data.forEach((el) => {
-      appendindData(el);
-    });
+    appendindData(data)
   } catch (err) {
     console.log(err);
   }
 };
 fetchData();
-let appendindData = (el) => {
-  let cards_container = document.querySelector("#content");
-  cards_container.innerHTML += ` <div class="card">
-    <div class="img-div">
-        <img id="image" src="${el.img}"
-            alt="">
-    </div>
-    <div class="card-content">
-        <div class="card-content-1">
-            <div>
-                <p class="blue"><span>${el.title}</span></p>
-            </div>
-            <div>${el.des}</div>
 
-        </div>
-        <div class="card-content-2">
-            <div class="card-price-btn">
-                <div>
-                    <p>As Low As</p>
-                    <p>$${el.prise}</p>
-                </div>
-                <div>
-                <a href="./payment.html"> 
-                <button id="${el.img}" class="${el.prise}">
-                    ADD TO BAG
-                </button>
-            </a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>`;
+
+let appendindData = (input) => {
+  let cards_container = document.querySelector("#content");
+  input.forEach((el)=>{
+    let cont_div= document.createElement("div")
+    cont_div.setAttribute("class","cart_div")
+
+    let img_div= document.createElement("div")
+    img_div.setAttribute("class","image_div")
+
+    let img= document.createElement("img")
+    img.setAttribute("class","image")
+    img.src=el.img
+    img_div.append(img)
+    let content_div=document.createElement("div")
+    content_div.setAttribute("class","cont_div")
+
+    let title_des_div =document.createElement("div")
+    title_des_div.setAttribute("class","title_des_div")
+
+    let title_div=document.createElement("div")
+    title_div.innerText=el.title
+    let des_div=document.createElement("div")
+    des_div.innerText=el.des
+    title_des_div.append(title_div,des_div)
+
+    let price_btn_div=document.createElement("div")
+    price_btn_div.setAttribute("class","prise_btn_div")
+    let price_div=document.createElement("div")
+    price_div.innerText=`As Low As ${el.prise}`
+    let btn= document.createElement("button")
+    btn.setAttribute("class","add_to_cart_btn")
+    btn.addEventListener("click", ()=>{
+      paymentPage(el)
+    })
+    btn.innerText="Add to Bag"
+    price_btn_div.append(price_div,btn)
+    content_div.append(title_des_div,price_btn_div)
+
+    cont_div.append(img_div,content_div)
+    cards_container.append(cont_div)
+  })
+  
 };
 
 
-let paymentPage=(event) => {
-  obj={
-    img:event.target.id,
-    prise:event.target.className
-  }
-  
+let paymentPage=(el) => {
+ let obj=el
+ console.log(obj)
   let data= JSON.parse(localStorage.getItem("obj")) || []
   data.push(obj)
   localStorage.setItem("obj",JSON.stringify(data))
-  window.location.href="/"
+  // window.location.href="/payment.html"
 }
